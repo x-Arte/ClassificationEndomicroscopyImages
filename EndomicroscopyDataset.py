82a3e288ea5db55bc6087111104c2214b679251d
x-Arte <zmyArte010828>
2023-11-16 23:41:25 +0000

    11/16 basic vgg train

--- EndomicroscopyDataset.py
+++ EndomicroscopyDataset.py
@@ -1,66 +1,37 @@
-from torch.utils.data import Dataset, random_split
-import os
 import torch
-from torch.utils.data import random_split,Dataset
 import os
-import torch
+import random
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+from EndomicroscopyImage import EndomicroscopyImage
 
 class EndomicroscopyDataset(Dataset):
-    def __init__(self, dataset):
-        self.dataset = dataset
+    def __init__(self, path, transform='vgg',shuffle=False):
+        super(EndomicroscopyDataset, self).__init__()
+        if not os.path.isdir(path):
+            raise NotADirectoryError
+        self.root = path
+        self.ls = os.listdir(path)
+        if shuffle:
+            random.shuffle(self.ls)
+        if transform == 'vgg':
+            self.transform = transforms.Compose([
+                #transforms.Grayscale(num_output_channels=3),  # gray to likeRGB
+                transforms.ToTensor(),
+                transforms.Resize((224, 224)),  # adjust the size
+                #transforms.Lambda(lambda x: x.repeat(3, 1, 1)),
+                transforms.Normalize(mean=[0.485], std=[0.229])
+            ])
+        #elif:
+        else:
+            self.transform = transform
 
-    def __len__(self):
-        return len(self.dataset)
 
     def __getitem__(self, idx):
-        """
-        get a data according to the idx
-        """
-        return self.dataset[idx]
+        img = torch.load(os.path.join(self.root, self.ls[idx]))
+        image = img.image
+        image = self.transform(image)
+        return image, img.label, img.filename
 
-def load_and_randomsplit_dataset(root_dir, train_size=0.8):
-    """
-    load EndomicroscopyImage data and split them into train dataset and test data set
-    :param root_dir: the path of .pt
-    :param train_size:
-    :return: train dataset and test dataset
-    """
-    items = []
-
-    # load EndomicroscopyImage
-    for file in os.listdir(root_dir):
-        if file.endswith('.pt'):
-            item = torch.load(os.path.join(root_dir, file))
-            items.append(item)
-
-    # split
-    train_len = int(len(items) * train_size)
-    test_len = len(items) - train_len
-    train_items, test_items = random_split(items, [train_len, test_len])
-
-    train_dataset = EndomicroscopyDataset(train_items)
-    test_dataset = EndomicroscopyDataset(test_items)
-
-    return train_dataset, test_dataset
-
-def save_dataset(dataset, file_path):
-    """
-    save the dataset into files
-    :param dataset:
-    :param file_path:
-    """
-    torch.save(dataset, file_path)
-
-if __name__ == "__main__":
-    root_dic = 'dataset/images/train/'
-    save_dic = 'dataset/'
-    train_size = 1
-    train_dataset, test_dataset = load_and_randomsplit_dataset(root_dic, train_size)
-    print("train_dataset size:"+str(train_dataset.__len__()))
-    print("test_dataset size:" + str(test_dataset.__len__()))
-    save_dataset(train_dataset,save_dic+'train1.dat')
-    save_dataset(test_dataset,save_dic+'test1.dat')
-
-
-
-
+    def __len__(self):
+        return len(self.ls)
---
+++ vgg19.py
@@ -0,0 +1,101 @@
+import torch.nn as nn
+from torchvision import models
+import torch
+from torch import optim, nn
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import Compose, Resize, ToTensor, Normalize
+from torch.utils.data import DataLoader
+import time
+import EndomicroscopyDataset
+from EndomicroscopyImage import EndomicroscopyImage
+
+def get_vgg19_model(pretrained=True, num_classes=2):
+    vgg19 = models.vgg19(pretrained=pretrained)
+    vgg19.features[0] = nn.Conv2d(1, 64, kernel_size=3, padding=1)
+    # Replace the classifier to match the number of classes
+    vgg19.classifier[6] = nn.Linear(vgg19.classifier[6].in_features, num_classes)
+    return vgg19
+
+
+def train(model, train_loader, test_loader, criterion, optimizer, epochs=25):
+    """
+    Train a VGG19 model.
+
+    Parameters:
+    - model (torch.nn.Module): The VGG19 model to train
+    - train_loader (torch.utils.data.DataLoader): DataLoader for the training set
+    - test_loader (torch.utils.data.DataLoader): DataLoader for the test set
+    - criterion: Loss function
+    - optimizer: Optimizer
+    - epochs (int): Number of training epochs
+    """
+    if torch.cuda.is_available():
+        print('yes gpu')
+    else:
+        print('oh god cpu')
+    device = torch.device('cuda')# if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
+    print("Start:"+str(start_time))
+
+    for epoch in range(epochs):
+        model.train()
+        running_loss = 0.0
+        for inputs, labels, name in train_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+        print(f"Epoch {epoch + 1}/{epochs} - Training loss: {running_loss / len(train_loader)}")
+
+        # Evaluate on the test set
+        model.eval()
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for inputs, labels, name in test_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        print(f'Accuracy of the network on the test images: {100 * correct // total} %')
+    print('Finished Training')
+
+
+if __name__ == '__main__':
+    # Hyperparameters
+    num_classes = 2  # Two classes
+    learning_rate = 0.001
+    batch_size = 8  # 16&64:orch.cuda.OutOfMemoryError: CUDA out of memory.
+    epochs = 25
+
+    # Transforms
+    transform = Compose([
+        Resize((100, 100)),  # Resize the image to 100x100
+        ToTensor(),
+        Normalize(mean=[0.5], std=[0.5])  # Normalize for a single channel
+    ])
+
+    # Datasets and DataLoaders
+    train_dataset = EndomicroscopyDataset.EndomicroscopyDataset('dataset/images/train')
+    train_loader = EndomicroscopyDataset.DataLoader(train_dataset, batch_size)
+
+    test_dataset = EndomicroscopyDataset.EndomicroscopyDataset('dataset/images/test')
+    test_loader = EndomicroscopyDataset.DataLoader(test_dataset, batch_size)
+
+
+
+    # Model, Loss, and Optimizer
+    vgg19 = get_vgg19_model(pretrained=True, num_classes=num_classes)  # Set pretrained=False for training from scratch
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(vgg19.parameters(), lr=learning_rate)
+
+    # Train the model
+    start_time = time.time()
+    train(vgg19, train_loader, test_loader, criterion, optimizer, epochs=epochs)
+    end_time = time.time()
+    print(f"Training time: {end_time - start_time}s")
