57b7962cc896036aa348437b59f33cce3c23e2a5
x-Arte <zmyArte010828>
2023-12-12 14:26:38 +0000

    12/12 transfer learning

---
+++ meat.py
@@ -0,0 +1,75 @@
+import time
+import torch
+import os
+
+from torch import nn, optim
+import cv2
+from EndomicroscopyImage import EndomicroscopyImage
+import EndomicroscopyDataset
+from vgg_pretrained import get_vgg19_model
+from train import train
+
+def get_saved_model(modelpath,num_classes):
+    model = get_vgg19_model()
+    model.load_state_dict(torch.load(modelpath))
+    for i in model.parameters():
+        i.requires_grad = False
+    # Replace the classifier to match the number of classes
+    model.classifier = nn.Sequential(
+        model.classifier[0],
+        model.classifier[1],
+        model.classifier[2],
+        model.classifier[3],
+        model.classifier[4],
+        model.classifier[5],
+        nn.Linear(model.classifier[6].in_features, model.classifier[6].in_features),
+        nn.ReLU(inplace=True),
+        nn.Dropout(p=0.25, inplace=False),
+        nn.Linear(model.classifier[6].out_features, num_classes)
+    )
+    model.classifier[9] = nn.Linear(model.classifier[6].out_features, num_classes)
+    print(model)
+    return model
+def save_pt(path, label, outputpath):
+    if not os.path.isdir(path):
+        raise NotADirectoryError
+    cnt = 0
+    for filename in os.listdir(path):
+        temp_path = path + '/' + filename
+        img = cv2.imread(temp_path, 0)
+        save_label = -1
+        if label == "beef":
+            save_label = 0
+        elif label == "ck":
+            save_label = 1
+        elif label == "pork":
+            save_label = 2
+        eimg = EndomicroscopyImage(label+filename[:filename.rfind('.')], cnt, save_label, img)
+        eimg.save(outputpath)
+        cnt += 1
+    print(cnt)
+
+if __name__ == "__main__":
+    #save_pt("E:\OneDrive\IC\group project\\new_dataset_pcle (2)\\new_dataset_pcle\\pork\\train","pork","meat/dataset/train/")
+    modelpath = "model/2023-12-01-01-04.pt"
+    model = get_saved_model(modelpath,3)
+    # Hyperparameters
+    num_classes = 3  # Two classes
+    learning_rate = 0.0001
+    batch_size = 64
+    epochs = 75
+    dropout = 0.20  # Resnet don‘t have to change
+
+    # Datasets and DataLoaders
+    train_dataset = EndomicroscopyDataset.EndomicroscopyDataset('meat/dataset/train/')
+    train_loader = EndomicroscopyDataset.DataLoader(train_dataset, batch_size)
+
+    test_dataset = EndomicroscopyDataset.EndomicroscopyDataset('meat/dataset/test/')
+    test_loader = EndomicroscopyDataset.DataLoader(test_dataset, batch_size)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    criterion = nn.CrossEntropyLoss()
+    # Train the model
+    start_time = time.time()
+    train(model, train_loader, test_loader, criterion, optimizer, epochs=epochs)
+    end_time = time.time()
+    print(f"Training time: {end_time - start_time}s")
--- train.py
+++ train.py
@@ -1,17 +1,15 @@
 import pandas as pd
-import torch.nn as nn
-from torchvision import models
 import torch
 from torch import optim, nn
-from torchvision.datasets import ImageFolder
-from torchvision.transforms import Compose, Resize, ToTensor, Normalize
-from torch.utils.data import DataLoader
+from sklearn.metrics import roc_auc_score
+from sklearn.preprocessing import label_binarize
 import time
 import EndomicroscopyDataset
 import vgg_pretrained
 import ResNet
+import numpy as np
 
-def train(model, train_loader, test_loader, criterion, optimizer, epochs=25):
+def train(model, train_loader, test_loader, criterion, optimizer, epochs=25, num_classes = 2):
     """
     Train a model.
 
@@ -29,42 +27,68 @@ def train(model, train_loader, test_loader, criterion, optimizer, epochs=25):
         print('oh god cpu')
     device = torch.device('cuda')# if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
-    print("Start:"+str(start_time))
+    #print("Start:"+str(start_time))
     tot_data = []
 
     for epoch in range(epochs):
         model.train()
+        train_correct = 0
+        train_total = 0
         running_loss = 0.0
+        testing_loss = 0.0
         for inputs, labels, name in train_loader:
             inputs, labels = inputs.to(device), labels.to(device)
             optimizer.zero_grad()
             outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            train_total += labels.size(0)
+            train_correct += (predicted == labels).sum().item()
+            # forward
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
 
-        print(f"Epoch {epoch + 1}/{epochs} - Training loss: {running_loss / len(train_loader)}")
+        print(f"----------------------- \n Epoch {epoch + 1}/{epochs} - Training loss: {running_loss / len(train_loader)} - Accuracy: {100 * train_correct / train_total} %")
 
         # Evaluate on the test set
         model.eval()
-        correct = 0
-        total = 0
+        # calculate the accuracy
+        test_correct = 0
+        test_total = 0
+        # calculate auc
+        all_labels = []
+        all_outputs = []
         with torch.no_grad():
             for inputs, labels, name in test_loader:
                 inputs, labels = inputs.to(device), labels.to(device)
                 outputs = model(inputs)
                 _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
-        print(f'Accuracy of the network on the test images: {100 * correct / total} %')
-        tot_data.append([epoch,running_loss / len(train_loader), 100 * correct / total])
+                test_total += labels.size(0)
+                test_correct += (predicted == labels).sum().item()
+
+                loss = criterion(outputs, labels)
+                testing_loss += loss.item()
+                all_labels.extend(labels.cpu().numpy())
+                # logits to possibility
+                probabilities = torch.nn.functional.softmax(outputs, dim=1).cpu().numpy()
+                all_outputs.extend(probabilities)
+        if num_classes == 2:
+            # 2
+            auc_score = roc_auc_score(all_labels, np.array(all_outputs)[:, 1])
+        else:
+            # >= 2
+            all_labels_binarized = label_binarize(all_labels, classes=range(num_classes))
+            auc_score = roc_auc_score(all_labels_binarized, np.array(all_outputs), multi_class='ovr')
+        print(f' Testing loss: {testing_loss / len(test_loader)} Accuracy: {100 * test_correct / test_total} % - Test AUC: {auc_score}')
+
+        tot_data.append([epoch,running_loss / len(train_loader),testing_loss / len(test_loader), 100 * train_correct / train_total, 100 * test_correct / test_total, auc_score])
 
     print('Finished Training')
     print('Wait to save')
 
     torch.save(model.state_dict(),'model/'+time.strftime('%Y-%m-%d-%H-%M', time.localtime())+'.pt')
-    df = pd.DataFrame(tot_data, columns=['epoch', 'training loss', 'accuracy'])
+    df = pd.DataFrame(tot_data, columns=['epoch', 'training loss', 'testing loss', 'train accuracy', 'test accuracy', 'AUC score'])
     df.to_csv('model/acc/' + time.strftime('%Y-%m-%d-%H-%M', time.localtime())+'.csv')
 
 if __name__ == '__main__':
@@ -72,7 +96,7 @@ if __name__ == '__main__':
     num_classes = 2  # Two classes
     learning_rate = 0.0001
     batch_size = 64
-    epochs = 80
+    epochs = 100
     dropout = 0.20 # Resnet don‘t have to change
 
     # Datasets and DataLoaders
